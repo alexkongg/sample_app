58dc8c9f54332da98073e6dbe9b0a6b993e7fe2a
Alex Kong <alex.kong91@gmail.com>
2012-06-04 16:08:23 -0700

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
-  has_secure_password
   
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
   
   validates :name, presence: true, length: { maximum: 50 }
   
@@ -22,8 +21,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false } 
                     
-  validates :password, presence: true, length: { minimum: 6 }
-  
+  validates :password, length: { minimum: 6 }
+  has_secure_password
   validates :password_confirmation, presence: true
  
   
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Alex Kong"
+    email    "alex@example.com"
+    password "173710"
+    password_confirmation "173710"
+  end
+end
